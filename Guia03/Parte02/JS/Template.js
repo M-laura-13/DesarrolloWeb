ecefe8ac82e2307c2807e76acab52d189b5f3464
Laura <mlcontrerast@ufpso.edu.co>
2025-09-20 13:53:34 -0600

    Parte 3

--- Guia03/Parte02/JS/Template.js
+++ Guia03/Parte02/JS/Template.js
@@ -2,9 +2,9 @@ const template = document.getElementById("card-template");
 const contenedor = document.getElementById("contenedor");
 
 const productos = [
-    { titulo: "Laptop", desc: "16GB RAM", price:"$1000", Image: "https://e7.pngegg.com/pngimages/789/503/png-clipart-rog-strix-scar-edition-gaming-laptop-gl503-rog-phone-republic-of-gamers-computex-laptop-electronics-netbook.png"}, //objetos, el titulo son atributos
-    { titulo: "Mouse", desc: "wireless", price: "$20", image: "https://png.pngtree.com/png-vector/20241227/ourlarge/pngtree-cute-mouse-holding-a-gift-spreading-joy-and-holiday-spirit-effortlessly-png-image_14919271.png" },
-    { titulo: "Keyborard", desc: "Wired", price: "$50", image: "https://w7.pngwing.com/pngs/38/977/png-transparent-computer-keyboard-computer-mouse-gaming-keypad-computer-cases-housings-backlight-computer-mouse-electronics-computer-keyboard-computer.png" } ,
+    { titulo: "Laptop", desc: "16GB RAM", price:"$1000", image: "https://www.tuexperto.com/wp-content/uploads/2012/12/M5-3.jpg"}, //objetos, el titulo son atributos
+    { titulo: "Mouse", desc: "wireless", price: "$20", image: "https://img.freepik.com/foto-gratis/raton-morado-ordenador_1260-11.jpg?semt=ais_incoming&w=740&q=80" },
+    { titulo: "Keyborard", desc: "Wired", price: "$50", image: "https://www.invidcomputers.com/images/000000000041235733790scorpion-k8-sin-fondo.png" } ,
     { titulo: "Sreen", desc: "LED", price: "$200", image: "https://dlcdnwebimgs.asus.com/gain/98c2391e-b168-48b5-bc9c-ac2c1dd429ba/w800" },
     { titulo: "CPU", desc: "x86/x64", price: "$500", image: "https://upload.wikimedia.org/wikipedia/commons/9/9a/AVADirect-Custom-X99-Intel-Core-i7-gaming-cpu.png" }
     
@@ -15,6 +15,6 @@ productos.forEach(producto => {
     clon.querySelector(".title").textContent = producto.titulo;
     clon.querySelector(".desc").textContent = producto.desc;
     clon.querySelector(".price").textContent = producto.price;
-    clon.querySelector(".image").textContent = producto.image;
+    clon.querySelector(".image").src = producto.image;
     contenedor.appendChild(clon);
 });
